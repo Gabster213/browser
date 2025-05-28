fad67023ff747a27a13bd44bec395d9bc268fe6a
Gabby Millatiner <gabby.millatiner@island.io>
2025-05-28 15:58:06 +0300

    Initial Commit

---
+++ browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  browser
+//
+//  Created by Gabriel Millatiner on 28/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ browserTests/browserTests.swift
@@ -0,0 +1,17 @@
+//
+//  browserTests.swift
+//  browserTests
+//
+//  Created by Gabriel Millatiner on 28/05/2025.
+//
+
+import Testing
+@testable import browser
+
+struct browserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
